6d637e4fd02be3623586fcb8db5061a4060237f6
Hafiz Ahmed Waseem <ahmed.waseem@aiforlife.ai>
2024-12-12 02:45:13 +0500

    Updated the mongodb connection file

--- mongoC.js
+++ mongoC.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
-
-const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+// const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const password = encodeURIComponent("9hmedw9seem");
 const connectionString = `mongodb+srv://hafizahmedwaseem:${password}@devcluster.2zzgx.mongodb.net/?retryWrites=true&w=majority`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
